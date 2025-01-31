c06222fba67da2834e85a874cebc94e2bdf092da
UlianaAsadulina <u.asadulina@icloud.com>
2025-01-31 16:17:18 -0500

    One model and route for Ingredients Collection

--- server.js
+++ server.js
@@ -1,11 +1,38 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 
+require('dotenv').config();
+//import models
+const Ingredient = require("./models/ingredient.js");
+
+const app = express();
 const PORT = 3000;
+
+
+
+//MIDDLEWARE
+//connect mongodb using connection string from .env
+mongoose.connect(process.env.MONGODB_URI);
+//message then terminal starts
+mongoose.connection.on("connected", ()=> {
+    //name of database what connected
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`);
+});
+
+
+
 
 app.get("/", (req, res) => {
     res.send("HOME");
 }); 
+
+
+// Get all ingredients
+app.get("/ingredients", async (req, res) => {      
+    let ingredients = await Ingredient.find({});  
+    res.send(ingredients);
+});
+
 
 app.listen(PORT, (req, res) => {
     console.log("Server starts on port "+PORT);
